f23ebd13b6972028f3ca3bb9d9224083bdde1549
Alex Gittemeier <algitt65@gmail.com>
2013-03-03 03:03:19 -0600

    Added basic ui controlling code

---
+++ device-sensor-streamer/SensorSettingsController.h
@@ -0,0 +1,29 @@
+//
+//  SensorSettingsController.h
+//  SensorStreamer
+//
+//  Created by Alex Gittemeier on 3/2/2013.
+//  Copyright (c) 2013 Gatormeier Business Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorSettingsController : UITableViewController<UITextFieldDelegate>
+#pragma mark Actions
+
+- (IBAction)udpModeChanged:(UISegmentedControl *)sender;
+- (IBAction)settingsChanged;
+
+#pragma mark -
+#pragma mark Outlets
+
+@property (weak, nonatomic) IBOutlet UISegmentedControl *udpMode;
+@property (weak, nonatomic) IBOutlet UITextField *targetAddress;
+@property (weak, nonatomic) IBOutlet UISwitch *shouldSendAccelerometerData;
+@property (weak, nonatomic) IBOutlet UITextField *accelerometerPort;
+@property (weak, nonatomic) IBOutlet UISwitch *shouldSendCameraData;
+@property (weak, nonatomic) IBOutlet UITextField *cameraPort;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *cameraSource;
+
+#pragma mark -
+@end
---
+++ device-sensor-streamer/SensorSettingsController.m
@@ -0,0 +1,89 @@
+    //
+    //  SensorSettingsController.m
+    //  SensorStreamer
+    //
+    //  Created by Alex Gittemeier on 3/2/2013.
+    //  Copyright (c) 2013 Gatormeier Business Consulting. All rights reserved.
+    //
+
+#import "SensorSettingsController.h"
+#import "RegexKitLite.h"
+
+@interface SensorSettingsController()
+
+@end
+
+@implementation SensorSettingsController
+#pragma mark Action handlers
+
+- (IBAction)udpModeChanged:(UISegmentedControl *)sender {
+        // Update state state of the address field, but nothing else
+    switch ([sender selectedSegmentIndex]) {
+        case 0:
+            self.targetAddress.enabled = YES;
+            self.targetAddress.textColor = [UIColor blackColor];
+            break;
+        case 1:
+            self.targetAddress.enabled = NO;
+            self.targetAddress.textColor = [UIColor darkGrayColor];
+        default:
+            break;
+    }
+
+    [self settingsChanged];
+}
+
+- (IBAction)settingsChanged {
+        // Run validation, and store to database
+    [self validateAndApplyInput];
+    [self storeInput];
+
+}
+
+#pragma mark -
+#pragma mark Validation methods
+
+- (void)validateAndApplyInput {
+    NSInteger mode = self.udpMode.selectedSegmentIndex;
+    NSString* address = self.targetAddress.text;
+
+    BOOL sendAcceleormeterData  = self.shouldSendAccelerometerData.on;
+    NSString* accelerometerPort = self.accelerometerPort.text;
+
+    BOOL sendCameraData = self.shouldSendCameraData.on;
+    NSString* cameraPort = self.cameraPort.text;
+    NSInteger cameraSource = self.cameraSource.selectedSegmentIndex;
+        //TODO: implement validation
+
+        //TODO: put all other info into model object
+}
+
+#pragma mark -
+#pragma mark DB interface methods
+
+-(void) storeInput {
+        //TODO: store in DB model object
+}
+
+#pragma mark -
+#pragma mark UI controller methods
+
+- (void)viewDidUnload {
+    [self setTargetAddress:nil];
+    [self setUdpMode:nil];
+    [self setShouldSendAccelerometerData:nil];
+    [self setAccelerometerPort:nil];
+    [self setShouldSendCameraData:nil];
+    [self setCameraPort:nil];
+    [self setCameraSource:nil];
+    [super viewDidUnload];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField*)sender
+{
+    [sender resignFirstResponder];
+    return YES;
+}
+
+#pragma mark -
+@end
