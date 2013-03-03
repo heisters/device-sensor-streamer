3b4ee3d1d3c98d912f5a79cbaa74b5d32be8f33c
Alex Gittemeier <algitt65@gmail.com>
2013-03-02 20:03:12 -0600

    Initial Commit

---
+++ device-sensor-streamer/SensorAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SensorAppDelegate.h
+//  device-sensor-streamer
+//
+//  Created by Alex Gittemeier on 3/2/2013.
+//  Copyright (c) 2013 Gatormeier Business Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SensorAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ device-sensor-streamer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  device-sensor-streamer
+//
+//  Created by Alex Gittemeier on 3/2/2013.
+//  Copyright (c) 2013 Gatormeier Business Consulting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SensorAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SensorAppDelegate class]));
+    }
+}
